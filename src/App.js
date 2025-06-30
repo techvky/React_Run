e2633ded393e36fa5fefd3f29aa8d748c3eec88f
techvky <60184998+techvky@users.noreply.github.com>
2025-06-30 16:03:24 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a minimal React app without CRA.</p>
+    </div>
+  );
+}
+
+export default App;
